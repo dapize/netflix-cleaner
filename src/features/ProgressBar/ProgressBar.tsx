c5d8e92e2a55cbe01b275f081439c09555cc3cd7
dapize <danielpz@outlook.com>
2025-09-05 21:12:04 +0200

    feat(ProgressBar): Add navigation by click and circle element

--- src/features/ProgressBar/ProgressBar.tsx
+++ src/features/ProgressBar/ProgressBar.tsx
@@ -1,5 +1,9 @@
+/** biome-ignore-all lint/a11y/noStaticElementInteractions: <explanation> */
+/** biome-ignore-all lint/a11y/useKeyWithClickEvents: <explanation> */
 import { type IMainContext, MainContext } from "@context/Main";
+import { Seek } from "@services/Seek";
 import { formatLeftTime } from "@utils/formatLeftTime";
+import type { MouseEvent } from "preact/compat";
 import { useCallback, useContext, useEffect, useState } from "preact/hooks";
 
 export const ProgressBar = () => {
@@ -30,12 +34,32 @@ export const ProgressBar = () => {
 		};
 	}, []);
 
+	const handleOnClickProgress = (event: MouseEvent<HTMLDivElement>) => {
+		const target = event.target as HTMLDivElement;
+		const rect = target.getBoundingClientRect();
+		const clickX = event.clientX - rect.left;
+		const width = rect.width;
+		const percentage = clickX / width;
+		const realTime = percentage * (videoNode as HTMLVideoElement).duration;
+		Seek(realTime * 1000);
+	};
+
 	return (
-		<div class="flex justify-between items-center w-full">
-			<div class="h-[6px] bg-white/30 rounded-[3px] relative w-[calc(100%-90px)]">
-				<div class="h-full rounded-[3px] bg-[#e50914]" style={{ width: `${percentage}%` }}></div>
+		<div class="flex justify-between items-center w-full gap-x-4">
+			<div
+				class="w-full pointer-events-auto h-[30px] flex justify-center items-center group hover:cursor-pointer"
+				onClick={handleOnClickProgress}
+			>
+				<div class="h-[6px] bg-white/30 rounded-[3px] relative w-full transition-all duration-200 group-hover:h-[10px]">
+					<div class="h-full rounded-[3px] bg-[#e50914] absolute" style={{ width: `${percentage}%` }}>
+						<button
+							class="h-[20px] w-[20px] rounded-[50%] bg-[#e50914] absolute right-[-10px] top-1/2 [transform:translateY(-50%)] hover:cursor-pointer"
+							type="button"
+						></button>
+					</div>
+				</div>
 			</div>
-			<span class="text-[20px] text-white shrink-0 w-[50px] flex justify-end items-center">{timeLeft}</span>
+			<span class="text-[18px] text-white shrink-0 w-[50px] flex justify-end items-center">{timeLeft}</span>
 		</div>
 	);
 };
--- src/utils/formatLeftTime.ts
+++ src/utils/formatLeftTime.ts
@@ -1,6 +1,10 @@
 export const formatLeftTime = (rawSeconds: number) => {
+	const view: string[] = [];
 	const hours = Math.floor(rawSeconds / 3600);
+	if (rawSeconds > 3600) view.push(`${hours.toString().padStart(2, "0")}`);
 	const minutes = Math.floor((rawSeconds - hours * 3600) / 60);
+	view.push(`${minutes.toString().padStart(2, "0")}`);
 	const seconds = rawSeconds - hours * 3600 - minutes * 60;
-	return `${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}:${Math.round(seconds)}`;
+	view.push(`${Math.round(seconds)}`);
+	return view.join(":");
 };
